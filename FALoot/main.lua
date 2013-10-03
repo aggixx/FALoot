9aa0faab30367c04358380d80566c02e888a6baf
aggixx <aggixx@yahoo.com>
2013-10-03 00:06:19 -0700

    Add libs/CallbackHandler-1.0

--- FALoot/main.lua
+++ FALoot/main.lua
@@ -5,7 +5,7 @@
 
 -- Declare strings
 local ADDON_NAME = "FALoot";
-local ADDON_VERSION_FULL = "v4.2c";
+local ADDON_VERSION_FULL = "v4.2d";
 local ADDON_VERSION = string.gsub(ADDON_VERSION_FULL, "[^%d]", "");
 
 local ADDON_COLOR = "FFF9CC30";
