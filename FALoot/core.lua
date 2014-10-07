2ac51dafd197a2f3bc3fe515392c09247e4e3398
aggixx <aggixx@yahoo.com>
2014-10-07 12:39:55 -0500

    Allow registration of several methods to be called for a single unique event. Events now cannot be unregistered. Add messages.Trigger() function.

--- FALoot/core.lua
+++ FALoot/core.lua
@@ -6,7 +6,12 @@ local A = FALoot;
      "Object" Definition
      ======================================================= --]]
 
--- Define addon events functions
+--[[
+   Define addon events functions
+   
+   A single unique event may have several methods called when it is triggered. These methods
+   cannot be unregistered once registered.
+--]]
 A.events = {};
 local E = A.events;
 E.list = {};
@@ -15,27 +20,53 @@ E.Register = function(event, func)
   if type(event) ~= "string" then
     error("events.Register passed a non-string value for event");
     return;
+  else if type(func) ~= "function" then
+    error("events.Register passed a non-function value for func");
+    return;
   end
-  E.list[event] = func;
+  if not E.list[event] then
+    E.list[event] = {};
+  end
+  table.insert(E.list[event], func);
 end
 
-E.Trigger = function(event, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10)
+E.Trigger = function(event, ...)
   if E.list[event] then
-    E.list[event](arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10);
+    for i=1,#E.list[event] do
+      E.list[event][i](...);
+    end
   end
 end
 
--- Define messages, events subfunction
+--[[
+   Define messages, events subfunction
+
+   Each unique message type may be set to have exactly one methods called when it is triggered.
+   A method can be unregistered by calling the Unregister() method on the event.
+-]]
 A.messages = {};
 local M = A.messages;
 M.list = {};
 
 M.Register = function(event, func)
   if type(event) ~= "string" then
-    error("events.Register passed a non-string value for event");
+    error("messages.Register passed a non-string value for event");
+    return;
+  else if type(func) ~= "function" then
+    error("messages.Register passed a non-function value for func");
     return;
   end
   M.list[event] = func;
+end
+
+M.Unregister = function(event)
+  M.list[event] = nil;
+end
+
+M.Trigger = function(mEvent, msg, channel, source, ...)
+  if M.list[mEvent] then
+    M.list[mEvent](msg, channel, source, ...);
+  end
 end
 
 --[[
