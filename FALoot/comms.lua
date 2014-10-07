cc1eb201980eb3ede6be32ae016987ea56fa468c
aggixx <aggixx@yahoo.com>
2014-10-07 13:32:30 -0500

    [Comms] Clean up sendMessage function to be able to call the function with less clutter.

--- FALoot/comms.lua
+++ FALoot/comms.lua
@@ -5,40 +5,49 @@ local F = A.functions;
 local libSerialize = LibStub:GetLibrary("AceSerializer-3.0");
 local libEncode = libCompress:GetAddonEncodeTable();
 
+local bulkMessages = {};
+
 --[[ ==========================================================================
      Communication Functions
      ========================================================================== --]]
      
-F.sendMessage = function(prefix, text, distribution, target, prio, validateTarget)
-  --serialize
-  local serialized, msg = libSerialize:Serialize(text)
+F.sendMessage = function(dist, target, mType, ...)
+  -- Determine priority
+  local priority = "NORMAL";
+  for i=1,#bulkMessages do
+    if mType == bulkMessages[i] then
+      priority = "BULK";
+    end
+  end
+  
+  -- Prep data in table form
+  local data = {...};
+  data["type"] = mType;
+  
+  -- Serialize the data
+  local serialized, msg = libSerialize:Serialize(data)
   if serialized then
-    text = serialized
+    data = serialized
   else
     U.debug("Serialization of data failed!");
     return false, "serialization of data failed";
   end
   
-  --encode
-  local encoded, msg = libEncode:Encode(text)
+  -- Encode the data
+  local encoded, msg = libEncode:Encode(data)
   if encoded then
-    text = encoded
+    data = encoded
   else
     U.debug("Encoding of data failed!");
     return false, "encoding of data failed";
   end
   
-  -- make sure target is valid
-  if validateTarget and string.lower(distribution) == "WHISPER" then
+  -- Validate whisper target to prevent errors for the user.
+  if string.upper(dist) == "WHISPER" then
     local groupType = ("raid" and IsInRaid()) or "party";
     for i=1,GetNumGroupMembers() do
       if U.UnitName(groupType..i, true) == target then
         if not UnitIsConnected(groupType..i) then
-          local mType;
-          for i,v in pairs(text) do
-            mType = i;
-            break;
-          end
           U.debug("The target of message type "..(mType or "unknown")..' "'..target..'" is offline.', 2);
           return false, "target of message is offline";
         end
@@ -47,6 +56,7 @@ F.sendMessage = function(prefix, text, distribution, target, prio, validateTarge
     end
   end
   
-  A.stub:SendCommMessage(prefix, text, distribution, target, prio)
+  -- Send the prepared message
+  A.stub:SendCommMessage(A.MESSAGE_PREFIX, data, dist, target, prio)
   return true;
 end
